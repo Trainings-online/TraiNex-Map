daa5471d8b058e7e34c5a43e57584c4c5394eab2
Bennikla <84016680+Bennikla@users.noreply.github.com>
2021-05-17 12:49:48 +0000

    Deploying to gh-pages from master @ 4198f74737ae5b95c7b2b7fb358e01dbb43d07f1 🚀

--- Wood.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="Wood" tilewidth="32" tileheight="32" tilecount="384" columns="24">
- <image source="../Textures/Wood/Isometric Wood 1 - 128x128.png" width="768" height="512"/>
-</tileset>
